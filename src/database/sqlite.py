57654b2a0476308815bea4f83082dc2393e58031
Calum Chiddy <calum.chiddy@Calums-MacBook-Pro.local>
2026-06-16 15:18:28 +0100

    feat: add session factory for db interaction

--- src/database/sqlite.py
+++ src/database/sqlite.py
@@ -1,3 +1,6 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 engine = create_engine("sqlite:///./database/users.db", echo=True) # Create a SQLite database engine for the "users.db" file in the current directory
+
+Session = sessionmaker(bind=engine) # Create a session factory bound to the engine, which will be used to create sessions for interacting with the database

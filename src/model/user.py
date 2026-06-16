56151aa2bdc45dd4030003327e0e566e7de00e41
Calum Chiddy <calum.chiddy@Calums-MacBook-Pro.local>
2026-06-16 14:36:16 +0100

    docs: add comments to user model

--- src/model/user.py
+++ src/model/user.py
@@ -2,15 +2,15 @@ from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import String
 
-class Base(DeclarativeBase):
+class Base(DeclarativeBase): # Base class for SQLAlchemy models
     pass
 
-class User(Base):
+class User(Base): # SQLAlchemy model for the "users" table
 
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String)
-    email: Mapped[str] = mapped_column(String)
+    id: Mapped[int] = mapped_column(primary_key=True) # Primary key column for the "users" table
+    name: Mapped[str] = mapped_column(String) # Name column for the "users" table
+    email: Mapped[str] = mapped_column(String) # Email column for the "users" table
 
 

dee84fe399502fae05d8e57d98bc76a1e130ff3c
Calum Chiddy <cchiddy480@gmail.com>
2026-06-17 15:16:12 +0100

    feat: implement create_user function for user registration

---
+++ src/repository/user_repository.py
@@ -0,0 +1,14 @@
+from model.user import User
+from database.sqlite import Session
+
+def create_user(name, email):
+    session = Session() # Create a new session for interacting with the database
+
+    user = User(name=name, email=email)
+
+    session.add(user) # Add the new user to the session's transaction
+    session.commit() # Commit the transaction to save the new user to the database
+    session.close() # Close the session 
+
+    return user.id # Return the ID of the newly created user
+
